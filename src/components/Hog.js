b12c46f588703dae3c6f3106cc18628e953b6993
paolov1928 <ventura.paolo@gmail.com>
2019-01-11 13:31:05 +0000

    i will never commit

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-export default class Hog extends Component {
+class Hog extends Component {
   render() {
     return (
       <div className="hog">
         // <Hog />
-        <p className='hog-details'>
-          {this.props.allHogs}
-        </p>
+        // <p className='hog-details'>
+        //   {this.props.allHogs}
+        // </p>
       </div>
     )
   }
 }
+
+export default Hog;
--- src/components/HogContainer.js
+++ src/components/HogContainer.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Hog from './Hog'
 
-export default class HogContainer extends Component {
+class HogContainer extends Component {
+
   render() {
+    console.log(this.props)
+
     return (
+
       <div className="container">
       {
-      this.props.allHogs.map(h => <Hog allHogs={h} />)
+      // this.props.allHogs.map(h => <Hog allHogs={h} />)
       }
       </div>
     )
   }
 }
+
+export default HogContainer;
