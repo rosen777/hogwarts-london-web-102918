cfcdcc84aabb8eb4b6ac0da5611c0ea9495ff3b4
rosen777 <rosen.toshev@flatironschool.com>
2019-01-11 13:17:17 +0000

    Rendering on the page

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -4,16 +4,16 @@ export default class Hog extends Component {
   render() {
     return (
       <div className="hog">
-      {
-
-        // <p className='hog-details'>
-        //   {this.props.selectedHog.name}
-        //   {this.props.selectedHog.specialty}
-        //   {this.props.selectedHog.greased}
-        // </p>
 
 
-}
+        <p className='hog-details'>
+          {this.props.selectedHog.name}
+          {this.props.selectedHog.specialty}
+          {this.props.selectedHog.greased}
+        </p>
+
+
+
       </div>
     )
   }
